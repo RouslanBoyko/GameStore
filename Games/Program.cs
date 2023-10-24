647832c9b95b919a84a32bba6754ed42bf5269a1
rouslan <rouslan>
2023-10-24 20:35:17 +0200

    POST for creating a game

--- Games/Program.cs
+++ Games/Program.cs
@@ -1,12 +1,11 @@
 using Games.Entities;
-
 namespace Games
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
+            const string GetGameEndPointName = "GetGame";
             List<Game> games = new()
             {
                         new Game()
@@ -46,15 +45,26 @@ namespace Games
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
+            // get all the games endpoint
             app.MapGet("/games", () => games);
+
+            // get games By Id endpoint
             app.MapGet("/games/{id}", (int id) =>
             {
                 Game? game = games.Find(g => g.Id == id);
 
                 if (game == null) return Results.NotFound();
                 return Results.Ok(game);
-            });
+            }).WithName(GetGameEndPointName);
 
+            // post for creating a new game
+            app.MapPost("/games", (Game game) => 
+            {
+                game.Id = games.Max(g => g.Id) + 1;
+                games.Add(game);
+
+                return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
+            });
             app.Run();
         }
     }
