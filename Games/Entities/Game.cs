e6a1a335b9324c41f9048039e599ebed4c7c181e
rousl <rousl@DESKTOP-A4BB7RD>
2023-10-24 17:47:13 +0200

    Add project files.

---
+++ Games/Entities/Game.cs
@@ -0,0 +1,16 @@
+﻿namespace Games.Entities
+{
+    public class Game
+    {
+        #region props
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Genre { get; set; }
+        public required decimal Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public required string ImageUri { get; set; }
+        #endregion
+
+
+    }
+}
---
+++ Games/Program.cs
@@ -0,0 +1,54 @@
+using Games.Entities;
+
+namespace Games
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            List<Game> games = new()
+            {
+                        new Game()
+                        {
+                            Id = 1,
+                            Name = "The Witcher 3",
+                            Genre = "Rpg",
+                            Price = 30M,
+                            ReleaseDate = new DateTime(2015, 05, 31),
+                            ImageUri = "https://placehold.co/100"
+                        },
+
+                        new Game()
+                        { 
+                            Id = 2,
+                            Name = "The Elder Scrolls V: Skyrim",
+                            Genre = "Rpg",
+                            Price = 30M,
+                            ReleaseDate = new DateTime(2015, 05, 31),
+                            ImageUri = "https://placehold.co/100"
+                        },
+
+                        new Game() 
+                        {
+                            Id = 3,
+                            Name = "Project Zomboids",
+                            Genre = "Survival",
+                            Price = 18M,
+                            ReleaseDate = new DateTime(2012, 05, 31),
+                            ImageUri = "https://placehold.co/100"
+                        }
+
+
+            };
+
+
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
