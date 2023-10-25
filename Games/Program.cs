b2bcb7e9493f141246d5ca978bd94c0564056f67
Ruslan <rouslanboyko.webdeveloper@gmail.com>
2023-10-25 14:50:28 +0200

    refactoring code with a extention method for game endpoints.

--- Games/Program.cs
+++ Games/Program.cs
@@ -1,3 +1,4 @@
+using Games.EndPoints;
 using Games.Entities;
 namespace Games
 {
@@ -5,103 +6,10 @@ namespace Games
     {
         public static void Main(string[] args)
         {
-            const string GetGameEndPointName = "GetGame";
-            List<Game> games = new()
-            {
-                        new Game()
-                        {
-                            Id = 1,
-                            Name = "The Witcher 3",
-                            Genre = "Rpg",
-                            Price = 30M,
-                            ReleaseDate = new DateTime(2015, 05, 31),
-                            ImageUri = "https://placehold.co/100"
-                        },
-
-                        new Game()
-                        {
-                            Id = 2,
-                            Name = "The Elder Scrolls V: Skyrim",
-                            Genre = "Rpg",
-                            Price = 30M,
-                            ReleaseDate = new DateTime(2015, 05, 31),
-                            ImageUri = "https://placehold.co/100"
-                        },
-
-                        new Game()
-                        {
-                            Id = 3,
-                            Name = "Project Zomboid",
-                            Genre = "Survival",
-                            Price = 18M,
-                            ReleaseDate = new DateTime(2012, 05, 31),
-                            ImageUri = "https://placehold.co/100"
-                        }
-
-
-            };
-
-
+ 
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
-
-            var group = app.MapGroup("/games")
-                           .WithParameterValidation();
-
-            #region endpoints
-            // GET all the games 
-            group.MapGet("/", () => games);
-
-            // GET games By Id with verification if it exists
-            group.MapGet("/{id}", (int id) =>
-            {
-                Game? game = games.Find(g => g.Id == id);
-
-                if (game is null) return Results.NotFound();
-                return Results.Ok(game);
-            }).WithName(GetGameEndPointName);
-
-            // POST for creating a new game
-            group.MapPost("/", (Game game) =>
-            {
-                game.Id = games.Max(g => g.Id) + 1;
-                games.Add(game);
-
-                return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
-            });
-
-            // PUT Update an existing game by ID
-            group.MapPut("/{id}", (int id, Game updatedGame) =>
-            {
-                Game? existingGame = games.Find(g => g.Id == id);
-
-                if (existingGame is null) return Results.NotFound();
-
-                // Update the existing game with the new data
-                existingGame.Name = updatedGame.Name;
-                existingGame.Genre = updatedGame.Genre;
-                existingGame.Price = updatedGame.Price;
-                existingGame.ReleaseDate = updatedGame.ReleaseDate;
-                existingGame.ImageUri = updatedGame.ImageUri;
-
-                return Results.NoContent();
-            });
-
-            // Delete a game by ID
-            group.MapDelete("/{id}", (int id) =>
-            {
-                Game? gameToRemove = games.Find(g => g.Id == id);
-
-                if (gameToRemove is not null)
-                {
-                    games.Remove(gameToRemove);
-                    return Results.NoContent();
-                }
-                return Results.NotFound();
-         
-            });
-            #endregion
-
+            app.MapGamesEndpoints();
             app.Run();
         }
     }
