9cd04ddce395ec7f1a93f6299577f487a23f8512
rouslanBoyko <rouslanboyko.webdeveloper@gmail.com>
2023-10-27 14:03:12 +0200

    Setting EntityFramework repository

---
+++ Games/Repositories/EntityFrameworkRepository.cs
@@ -0,0 +1,40 @@
+﻿using Games.Data;
+using Games.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Games.Repositories;
+
+public class EntityFrameworkRepository : IGamesRepository
+{
+    private readonly GameStoreContext dbContext;
+
+    public EntityFrameworkRepository(GameStoreContext dbContext)
+    {
+        this.dbContext=dbContext;
+    }
+
+    public IEnumerable<Game> GetAll()
+    {
+        return dbContext.Games.AsNoTracking().ToList();
+    }
+
+    public Game? GetById(int id)
+    {
+        return dbContext.Games.Find(id);
+    }
+    public void Create(Game game)
+    {
+        dbContext.Games.Add(game);
+        dbContext.SaveChanges();
+    }
+
+    public void Update(Game updatedGame)
+    {
+        dbContext.Update(updatedGame);
+        dbContext.SaveChanges();
+    }
+    public void Delete(int id)
+    {
+        dbContext.Games.Where(game => game.Id == id).ExecuteDelete();
+    }
+}
